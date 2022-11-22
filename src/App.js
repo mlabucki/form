5c029d2792d4ad2250f6a2c1a146691a7a02680f
Michal Labucki <labucki.michal@gmail.com>
2022-11-22 15:12:21 +0100

    Working POC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const AT_LEAST_ONE_AT_SIGN_RE = /^[^@]*@{1}[^@]*$/;
+
+function validateForm(firstName, password, email, newsletterAllowed) {
+  const firstNameValid = firstName !== "";
+  const passwordValid = password !== "";
+  const emailValid = newsletterAllowed
+    ? AT_LEAST_ONE_AT_SIGN_RE.test(email) && email.includes(".")
+    : true;
+
+  return firstNameValid && passwordValid && emailValid;
+}
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [newsletterAllowed, setNewsletterAllowed] = useState(false);
+  const [isValid, setIsValid] = useState(undefined);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const valid = validateForm(firstName, password, email, newsletterAllowed);
+    setIsValid(valid);
+  };
+
+  const handleInputChange = (setter) => (e) => {
+    setter(e.target.value);
+  };
+
+  const handleNewsletterAllowedChange = () => {
+    setNewsletterAllowed(!newsletterAllowed);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={handleInputChange(setFirstName)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="text"
+          placeholder="Password"
+          value={password}
+          onChange={handleInputChange(setPassword)}
+        />
+        <label htmlFor="newsletterAllowed">Allow newsletter</label>
+        <input
+          id="newsletterAllowed"
+          type="checkbox"
+          name="newsletterAllowed"
+          checked={newsletterAllowed}
+          onChange={handleNewsletterAllowedChange}
+        />
+        {newsletterAllowed && (
+          <>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={handleInputChange(setEmail)}
+            />
+          </>
+        )}
+        <button>Submit</button>
+        {isValid === true && "Registered successfully"}
+        {isValid === false && "Validation error"}
+      </form>
     </div>
   );
 }
